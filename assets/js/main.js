ab6c331f9dd030ed2260a1c1c52552daf366f78b
lapu torres~ <lapu.torres@gmail.com>
2023-05-08 15:29:44 -0500

    Tournament Grand Price fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
-
-//Slider buttons add active class 
+//Slider buttons add active class
 
 const buttons = document.getElementsByClassName("btn-slider");
 const carousel = document.getElementById("main-carousel");
@@ -11,9 +10,8 @@ if (activeButton === null) {
   activeButton = document.querySelector(".btn-slider.active");
 }
 
-carousel.addEventListener("click", function(event) {
+carousel.addEventListener("click", function (event) {
   if (event.target.classList.contains("btn-slider")) {
-
     // Verificar si el botón que se hizo clic es diferente del botón activo actual
     if (event.target !== activeButton) {
       activeButton.classList.remove("active");
@@ -22,9 +20,25 @@ carousel.addEventListener("click", function(event) {
     }
   }
 });
-  
-  
-  
-  
-  
-  
+
+//Show list Tournament Grand Prize
+
+const tournamentButton = document.querySelectorAll(".view-more");
+const tournamentPrice = document.getElementsByClassName("grand-prize-item");
+
+tournamentButton.forEach(function (boton) {
+  boton.addEventListener("click", function () {
+    let lista = boton.parentNode.querySelector("ul");
+    let items = lista.querySelectorAll(".grand-prize-item");
+
+    items.forEach(function (item) {
+      if (item.classList.contains("d-none")) {
+        item.classList.remove("d-none");
+        boton.textContent = "Close";
+      } else {
+        item.classList.add("d-none");
+        boton.textContent = "View More";
+      }
+    });
+  });
+});
