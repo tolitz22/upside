6ff6995708c5ce50c8487459d9b4ecdfb2a2644f
angelito <tubigangelito981@gmail.com>
2024-05-19 05:17:30 +0800

    fix extra large spacing

--- assets/js/main.js
+++ assets/js/main.js
@@ -11,6 +11,7 @@ if (activeButton === null) {
 }
 
 carousel.addEventListener("click", function (event) {
+  
   if (event.target.classList.contains("btn-slider")) {
     // Verificar si el botón que se hizo clic es diferente del botón activo actual
     if (event.target !== activeButton) {
@@ -107,24 +108,234 @@ function handleSearch(input) {
 
 
   // Espera a que se cargue el documento
-  document.addEventListener('DOMContentLoaded', function() {
-    // Obtén el enlace "Leaderboards"
-    var leaderboardsLink = document.getElementById('leaderboards-link');
+  // document.addEventListener('DOMContentLoaded', function() {
+  //   // Obtén el enlace "Leaderboards"
+  //   var leaderboardsLink = document.getElementById('leaderboards-link');
     
-    // Agrega un evento de clic al enlace
-    leaderboardsLink.addEventListener('click', function(e) {
-      e.preventDefault(); // Evita el comportamiento de enlace predeterminado
+  //   // Agrega un evento de clic al enlace
+  //   leaderboardsLink.addEventListener('click', function(e) {
+  //     e.preventDefault(); // Evita el comportamiento de enlace predeterminado
       
-      // Obtén el destino del enlace
-      var target = document.querySelector(this.getAttribute('href'));
+  //     // Obtén el destino del enlace
+  //     var target = document.querySelector(this.getAttribute('href'));
       
-      // Calcula la posición del destino en relación con la ventana
-      var offset = target.getBoundingClientRect().top + window.pageYOffset;
+  //     // Calcula la posición del destino en relación con la ventana
+  //     var offset = target.getBoundingClientRect().top + window.pageYOffset;
       
-      // Realiza el desplazamiento suave utilizando animaciones de JavaScript
-      window.scrollTo({
-        top: offset,
-        behavior: 'smooth'
-      });
-    });
-  });
+  //     // Realiza el desplazamiento suave utilizando animaciones de JavaScript
+  //     window.scrollTo({
+  //       top: offset,
+  //       behavior: 'smooth'
+  //     });
+  //   });
+  // });
+
+
+
+//   function makeRequest() {
+//     // Create a new XMLHttpRequest object
+//     var xhr = new XMLHttpRequest();
+
+//     // Configure the request: GET method, URL
+//     xhr.open('GET', 'https://gettournamentleaderboards-4sbyyp2mpq-uc.a.run.app', true);
+
+//     // Set up a function to handle the response
+//     xhr.onload = function() {
+//         if (xhr.status >= 200 && xhr.status < 300) {
+//             // Success! Handle the response data
+//             var responseData = JSON.parse(xhr.responseText);
+//             var carouselInnerDiv = document.querySelector('#carouselExampleIndicators2 .carousel-inner');
+
+//             // Function to generate HTML for leaderboard section
+//             function generateLeaderboardHTML(title, prizeDescription, leaderboard) {
+//                 var html = `
+//                     <div class="carousel-item">
+//                         <div class="row">
+//                             <div class="col-md-4 card-container mb-3">
+//                                 <div class="grand-prize-container text-center">
+//                                     <p class="title">${title}</p>
+//                                     <div class="text d-flex">
+//                                         <ul class="grand-prize-list">
+//                                             <!-- Grand prize list goes here -->
+//                                         </ul>
+//                                     </div>
+//                                     <button class="view-more">View More</button>
+//                                 </div>
+//                                 <div class="card">
+//                                     <div class="card-body text-center">
+//                                         <h4 class="card-title">${title}</h4>
+//                                         <p>${prizeDescription}</p>
+//                                         <div class="table-container">
+//                                             <table class="text-start leaderboard-table">
+//                                                 <tr>
+//                                                     <th>Nickname</th>
+//                                                     <th>Tokens</th>
+//                                                 </tr>`;
+
+//                 // Generate table rows for leaderboard
+//                 leaderboard.forEach(entry => {
+//                     html += `
+//                                                 <tr>
+//                                                     <td>${entry.nickname}</td>
+//                                                     <td>${entry.tokens}</td>
+//                                                 </tr>`;
+//                 });
+
+//                 html += `
+//                                             </table>
+//                                         </div>
+//                                     </div>
+//                                 </div>
+//                             </div>
+//                         </div>
+//                     </div>`;
+
+//                 return html;
+//             }
+
+      
+
+//             // Loop through the tournaments and generate HTML for each leaderboard
+//             responseData.tournaments.forEach(tournament => {
+//                 var html = generateLeaderboardHTML(tournament.title, tournament.prizeDescription, tournament.leaderboard);
+//                 // console.log(html);
+//                 carouselInnerDiv.innerHTML += html;
+//             });
+
+//             // Activate the first carousel item if it exists
+//             var firstCarouselItem = carouselInnerDiv.querySelector('.carousel-item');
+//             if (firstCarouselItem) {
+//                 firstCarouselItem.classList.add('active');
+//             }
+       
+//         } else {
+//             // Request failed
+//             console.error('Request failed with status:', xhr.status);
+//         }
+//     };
+
+//     // Set up a function to handle errors
+//     xhr.onerror = function() {
+//         // Request error
+//         console.error('Request failed');
+//     };
+
+//     // Send the request
+//     xhr.send();
+// }
+
+// // Call the makeRequest function to fetch leaderboard data
+
+
+
+
+$(document).ready(function () {
+
+
+  function makeRequest() {
+    // Create a new XMLHttpRequest object
+    var xhr = new XMLHttpRequest();
+  
+    // Configure the request: GET method, URL
+    xhr.open('GET', 'https://gettournamentleaderboards-4sbyyp2mpq-uc.a.run.app', true);
+  
+    // Set up a function to handle the response
+    xhr.onload = function() {
+        if (xhr.status >= 200 && xhr.status < 300) {
+            // Success! Handle the response data
+            var responseData = JSON.parse(xhr.responseText);
+            var tournaments = responseData.tournaments;
+  
+            // Get the main carousel element
+            var mainCarousel = document.querySelector('.leaderboard-carousel');
+  
+            // Iterate through the tournaments and create a carousel cell for each one
+            tournaments.forEach(function(tournament) {
+                var carouselCell = document.createElement('div');
+                // carouselCell.classList.add('carousel-cell');
+                carouselCell.classList.add('card-container');
+                carouselCell.classList.add('col-md-4');
+                carouselCell.classList.add('mb-3');
+                
+                var leaderboardHTML = '';
+                tournament.leaderboard.forEach(function(entry) {
+                    leaderboardHTML += `
+                        <tr>
+                            <td>${entry.nickname}</td>
+                            <td>${entry.tokens}</td>
+                        </tr>
+                    `;
+                });
+  
+                var innerHTML = `
+                        <div class="grand-prize-container text-center">
+                            <p class="title">${tournament.title}</p>
+                            <div class="text d-flex">
+                                <ul class="grand-prize-list">
+                                    <li class="d-flex">
+                                        <p class="points">3000 pts</p>
+                                        <p class="name">Nantucket Red® polo</p>
+                                    </li>
+                                    <!-- Add more list items dynamically if needed -->
+                                </ul>
+                            </div>
+                            <button class="view-more">View More</button>
+                        </div>
+                        <div class="card">
+                            <div class="card-body text-center">
+                                <h4 class="card-title">Leaderboard</h4>
+                                <input type="text" class="search-input" placeholder="Search">
+                                <div class="table-container">
+                                    <table class="text-start leaderboard-table">
+                                        <tr>
+                                            <th>Username</th>
+                                            <th>Pts</th>
+                                        </tr>
+                                        ${leaderboardHTML}
+                                    </table>
+                                </div>
+                                <p class="mt-5">
+                                    Don't see your score? Please double-check your
+                                    username in the Profile section of the Upside app.
+                                </p>
+                            </div>
+                        </div>
+          
+                `;
+  
+                carouselCell.innerHTML = innerHTML;
+                mainCarousel.appendChild(carouselCell);
+  
+            });
+  
+            // Initialize Flickity after adding the carousel cells
+  
+            var elem = document.querySelector('.leaderboard-carousel');
+            var flkty = new Flickity( elem, {
+              // options
+              cellAlign: 'left',
+              contain: true,
+              pageDots: false,
+              groupCells:false,
+            });
+            
+
+        } else {
+            // Request failed
+            console.error('Request failed with status:', xhr.status);
+        }
+    };
+  
+    // Set up a function to handle errors
+    xhr.onerror = function() {
+        // Request error
+        console.error('Request failed');
+    };
+  
+    // Send the request
+    xhr.send();
+  }
+  makeRequest();
+
+
+})
