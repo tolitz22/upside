192c54b566fcd33afa2a620bc1cc5a00fe117273
angelito <tubigangelito981@gmail.com>
2024-06-06 02:17:58 +0800

    enhancements

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,44 @@ const buttons = document.getElementsByClassName("btn-slider");
 const carousel = document.getElementById("main-carousel");
 let activeButton = document.querySelector(".btn-slider.active");
 
-// Asegurarse de que haya un botón activo inicialmente
-if (activeButton === null && buttons[0] ) {
-  buttons[0].classList.add("active");
-  activeButton = document.querySelector(".btn-slider.active");
 
-  carousel.addEventListener("click", function (event) {
-  
-    if (event.target.classList.contains("btn-slider")) {
-      // Verificar si el botón que se hizo clic es diferente del botón activo actual
-      if (event.target !== activeButton) {
-        activeButton.classList.remove("active");
-        event.target.classList.add("active");
-        activeButton = event.target;
-      }
-    }
+
+document.addEventListener("DOMContentLoaded", function() {
+  const myCarouselElement = document.querySelector('#main-carousel')
+  const carousel = new bootstrap.Carousel(myCarouselElement, {
+    interval: 5000,   
+    pause:'hover',
+    ride:'carousel'
+  })
+
+  console.log(carousel);
+})
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  const desktopButtons = document.querySelectorAll('.btn-slider');
+  const mobileButtons = document.querySelectorAll('.buttons-slider-container-mobile .btn');
+  const allButtons = [...desktopButtons, ...mobileButtons];
+  let activeButton = document.querySelector('.btn-slider.active');
+
+  allButtons.forEach(button => {
+      button.addEventListener('click', function(event) {
+          const myCarouselEl = document.querySelector('#main-carousel');
+          const carousel = bootstrap.Carousel.getInstance(myCarouselEl);
+
+          if (carousel) {
+              carousel.dispose();
+          }
+
+          if (event.target !== activeButton) {
+              allButtons.forEach(btn => btn.classList.remove('active'));
+              event.target.classList.add('active');
+              activeButton = event.target;
+          }
+      });
   });
-}
-
+});
 
 
 //Show list Tournament Grand Prize
@@ -96,129 +116,6 @@ function handleSearch(input) {
 }
 
 
-
-  // Espera a que se cargue el documento
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   // Obtén el enlace "Leaderboards"
-  //   var leaderboardsLink = document.getElementById('leaderboards-link');
-    
-  //   // Agrega un evento de clic al enlace
-  //   leaderboardsLink.addEventListener('click', function(e) {
-  //     e.preventDefault(); // Evita el comportamiento de enlace predeterminado
-      
-  //     // Obtén el destino del enlace
-  //     var target = document.querySelector(this.getAttribute('href'));
-      
-  //     // Calcula la posición del destino en relación con la ventana
-  //     var offset = target.getBoundingClientRect().top + window.pageYOffset;
-      
-  //     // Realiza el desplazamiento suave utilizando animaciones de JavaScript
-  //     window.scrollTo({
-  //       top: offset,
-  //       behavior: 'smooth'
-  //     });
-  //   });
-  // });
-
-
-
-//   function makeRequest() {
-//     // Create a new XMLHttpRequest object
-//     var xhr = new XMLHttpRequest();
-
-//     // Configure the request: GET method, URL
-//     xhr.open('GET', 'https://gettournamentleaderboards-4sbyyp2mpq-uc.a.run.app', true);
-
-//     // Set up a function to handle the response
-//     xhr.onload = function() {
-//         if (xhr.status >= 200 && xhr.status < 300) {
-//             // Success! Handle the response data
-//             var responseData = JSON.parse(xhr.responseText);
-//             var carouselInnerDiv = document.querySelector('#carouselExampleIndicators2 .carousel-inner');
-
-//             // Function to generate HTML for leaderboard section
-//             function generateLeaderboardHTML(title, prizeDescription, leaderboard) {
-//                 var html = `
-//                     <div class="carousel-item">
-//                         <div class="row">
-//                             <div class="col-md-4 card-container mb-3">
-//                                 <div class="grand-prize-container text-center">
-//                                     <p class="title">${title}</p>
-//                                     <div class="text d-flex">
-//                                         <ul class="grand-prize-list">
-//                                             <!-- Grand prize list goes here -->
-//                                         </ul>
-//                                     </div>
-//                                     <button class="view-more">View More</button>
-//                                 </div>
-//                                 <div class="card">
-//                                     <div class="card-body text-center">
-//                                         <h4 class="card-title">${title}</h4>
-//                                         <p>${prizeDescription}</p>
-//                                         <div class="table-container">
-//                                             <table class="text-start leaderboard-table">
-//                                                 <tr>
-//                                                     <th>Nickname</th>
-//                                                     <th>Tokens</th>
-//                                                 </tr>`;
-
-//                 // Generate table rows for leaderboard
-//                 leaderboard.forEach(entry => {
-//                     html += `
-//                                                 <tr>
-//                                                     <td>${entry.nickname}</td>
-//                                                     <td>${entry.tokens}</td>
-//                                                 </tr>`;
-//                 });
-
-//                 html += `
-//                                             </table>
-//                                         </div>
-//                                     </div>
-//                                 </div>
-//                             </div>
-//                         </div>
-//                     </div>`;
-
-//                 return html;
-//             }
-
-      
-
-//             // Loop through the tournaments and generate HTML for each leaderboard
-//             responseData.tournaments.forEach(tournament => {
-//                 var html = generateLeaderboardHTML(tournament.title, tournament.prizeDescription, tournament.leaderboard);
-//                 // console.log(html);
-//                 carouselInnerDiv.innerHTML += html;
-//             });
-
-//             // Activate the first carousel item if it exists
-//             var firstCarouselItem = carouselInnerDiv.querySelector('.carousel-item');
-//             if (firstCarouselItem) {
-//                 firstCarouselItem.classList.add('active');
-//             }
-       
-//         } else {
-//             // Request failed
-//             console.error('Request failed with status:', xhr.status);
-//         }
-//     };
-
-//     // Set up a function to handle errors
-//     xhr.onerror = function() {
-//         // Request error
-//         console.error('Request failed');
-//     };
-
-//     // Send the request
-//     xhr.send();
-// }
-
-// // Call the makeRequest function to fetch leaderboard data
-
-
-
-
 $(document).ready(function () {
 
 
@@ -238,7 +135,12 @@ $(document).ready(function () {
   
             // Get the main carousel element
             var mainCarousel = document.querySelector('.leaderboard-carousel');
-  
+            
+            tournaments = tournaments.filter((item)=>{
+              return item.leaderboard.length>2
+            });
+
+            console.log(tournaments);              
             // Iterate through the tournaments and create a carousel cell for each one
             tournaments.forEach(function(tournament) {
                 var carouselCell = document.createElement('div');
@@ -294,8 +196,8 @@ $(document).ready(function () {
               // options
               cellAlign: 'left',
               contain: true,
-              pageDots: true,
-              groupCells:false,
+              pageDots: true,              
+              groupCells: tournaments.length>2? true:false,              
             });
             
 
